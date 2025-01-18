8780896b58e6ae4248a00577d794bf1cd5284377
Race Williams <race@racewilliams.com>
2025-01-17 20:40:19 -0500

    copy ci, remove previous setup

--- lua/tamagotchi/init.lua
+++ lua/tamagotchi/init.lua
@@ -1,21 +1,3 @@
 local M = {}
-local config = require("tamagotchi.config")
-local events = require("tamagotchi.events")
-local pet = require("tamagotchi.pet")
-
-function M.setup(user_opts)
-	config.setup(user_opts)
-
-	vim.api.nvim_set_keymap(
-		"n",
-		config.options.ui.keybind,
-		':lua require("tamagotchi.ui").toggle()',
-		{ noremap = true, silent = true }
-	)
-
-	pet.initialize(config.options.pet)
-
-	events.setup()
-end
 
 return M
