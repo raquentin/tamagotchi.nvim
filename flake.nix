7e6a22e04d038f468e5d5832c47aaecf6fb50206
Race Williams <race@raquent.in>
2025-10-17 23:39:10 -0400

    remove tests

--- flake.nix
+++ flake.nix
@@ -20,7 +20,6 @@
             pkgs.lua
             pkgs.luarocks
             luaPkgs.luacheck
-            luaPkgs.busted
             luaPkgs.luaunit
             pkgs.stylua
             pkgs.git
@@ -29,19 +28,6 @@
           shellHook = ''
             echo
             echo "tamagotchi.nvim dev shell"
-            echo "Lint:"
-            echo "  luacheck lua/ plugin/"
-            echo "  stylua --check .      # or: stylua .   (to format)"
-            echo
-            echo "tests (pick your runner):"
-            echo "  # If using Busted (files like *_spec.lua):"
-            echo "  busted"
-            echo
-            echo "  # If using LuaUnit (files like test_*.lua):"
-            echo "  lua -e \"luaunit=require('luaunit'); os.exit(luaunit.LuaUnit.run())\" test/"
-            echo
-            echo "headless Neovim (Plenary+Busted style):"
-            echo "  nvim --headless -c \"lua require('plenary.busted').run()\" -c qa"
             echo
           '';
         };
